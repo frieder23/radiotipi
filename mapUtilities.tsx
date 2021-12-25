6a2cb878dd34976f1ea139e8685e6ccced7d0223
frieder23 <96354381+frieder23@users.noreply.github.com>
2021-12-25 23:58:20 +0100

    floor

--- mapUtilities.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="mapUtilities" tilewidth="32" tileheight="32" tilecount="9" columns="9">
- <image source="assets/mapUtilities.png" trans="ff00ff" width="288" height="32"/>
- <tile id="1">
-  <properties>
-   <property name="startLayer" type="bool" value="true"/>
-  </properties>
- </tile>
-</tileset>
