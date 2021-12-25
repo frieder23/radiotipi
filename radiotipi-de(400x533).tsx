a83edfc1bd6ae173d31796bb59a067af0e38e186
frieder23 <96354381+frieder23@users.noreply.github.com>
2021-12-25 23:46:04 +0100

    assets

---
+++ radiotipi-de(400x533).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="radiotipi-de(400x533)" tilewidth="32" tileheight="32" tilecount="192" columns="12">
+ <image source="assets/radiotipi-de(400x533).png" trans="ff00ff" width="400" height="533"/>
+</tileset>
