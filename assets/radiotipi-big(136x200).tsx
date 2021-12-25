01dea81cdd394c8220b2acfacf65072020b027bb
frieder23 <96354381+frieder23@users.noreply.github.com>
2021-12-25 22:40:46 +0100

    upload

---
+++ assets/radiotipi-big(136x200).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="radiotipi-big(136x200)" tilewidth="32" tileheight="32" tilecount="24" columns="4">
+ <image source="../../maps/radiotipi-big(136x200).png" trans="ff00ff" width="136" height="200"/>
+</tileset>
