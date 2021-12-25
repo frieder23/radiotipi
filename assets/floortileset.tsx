2d045287249f378ee954023474a73500ac9cfda8
frieder23 <96354381+frieder23@users.noreply.github.com>
2021-12-25 23:29:20 +0100

    alles neu

--- assets/floortileset.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="floortileset" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="49" columns="7">
- <image source="../../maps/workadventure-map-starter-kit-master/floortileset.png" trans="ff00ff" width="256" height="256"/>
- <tile id="8">
-  <properties>
-   <property name="collides" type="bool" value="true"/>
-  </properties>
- </tile>
-</tileset>
--- assets/tipi 300x400.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="tipi 300x400" tilewidth="32" tileheight="32" tilecount="108" columns="9">
- <image source="../../maps/vorlagen-radiotipi/radiotipi-de(300x400).png" trans="ff00ff" width="300" height="400"/>
-</tileset>
